cda031f970b721176c405a800057e2ab6d6d8946
Md Akmal Hossain <akmal2080@gmail.com>
2019-07-26 00:31:48 -0400

    build folder

---
+++ client/build/precache-manifest.5f300272e38467ca405c72ef34f0b274.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab989a0aa1903999dea168d7d1e2402a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e7d53b5c461b6665033",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "be4fff345fe5df8df8ed",
+    "url": "/static/js/2.e3588019.chunk.js"
+  },
+  {
+    "revision": "3e7d53b5c461b6665033",
+    "url": "/static/js/main.a4ae27a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.get('/', function(req, res) {
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    // app.get("*", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "/client/build/index.html"));
-    // });
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "/client/build/index.html"));
+    });
 }
 
 
