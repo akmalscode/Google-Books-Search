0ea619390dfca396c7aeab8bc18beeba5122d354
Md Akmal Hossain <akmal2080@gmail.com>
2019-07-26 00:26:06 -0400

    path

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ app.post("/api/books",function(req,res){
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "/client/build/index.html"));
+    // });
 }
 
 
