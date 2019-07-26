74cf56e3704efd041623cffc4f92fa78f5eef43f
Md Akmal Hossain <akmal2080@gmail.com>
2019-07-25 23:33:25 -0400

    server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI = PORT = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+
+mongoose.client(MONGODB_URI, { useNewUrlParser: true });
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+}
+
+
+app.listen(PORT);
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './BookSearch';
+import SavedBooks from "./SavedBooks";
+import './App.css';
+
+class App extends Component {
+
+render(){
+
+return(){
+  <Router>
+    <nav>
+      <ul>
+        <li>
+          
+        </li>
+      </ul>
+    </nav>
+
+
+  </Router>
+}
+
+}
+
+}
+
+export default App;
